e06521e777e6af1a1e38b971ef143d40dd9292c3
James Woolfenden <james.woolfenden@gmail.com>
2023-06-19 08:15:06 +0100

    refresh

--- hash.sh
+++ hash.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # gets hash of tag
-if [ $# -eq 0 ]
+if [[ $# -eq 0 ]]
   then
     version=$(git describe --tags --abbrev=0)
 else
@@ -8,41 +8,41 @@ else
 fi
 
 #shellcheck disable=SC2046,SC2005
-echo $(git show-ref --tag "$version") | tail -1| head -n1 | cut -d " " -f1
+echo $(git show-ref --tag "${version}") | tail -1| head -n1 | cut -d " " -f1
 
 function get-moduleversion {
  IFS='/' read -r name provider module <<<"$1"
  url="https://github.com/${name}/terraform-${provider}-${module}"
  git -c 'versionsort.suffix=-' \
-    ls-remote --exit-code --refs --sort='version:refname' --tags "$url" '*.*.*' \
+    ls-remote --exit-code --refs --sort='version:refname' --tags "${url}" '*.*.*' \
     | tail --lines=1 \
     | cut --delimiter='/' --fields=3
 }
 
 function get-moduleinfo() {
-  IFS='/' read -r name provider module <<<"$1"
+  IFS='/' read -r name provider module <<<"${1}"
   url="https://github.com/${name}/terraform-${provider}-${module}"
   #echo $url
   results=$(git -c 'versionsort.suffix=-' \
-    ls-remote --exit-code --refs --sort='version:refname' --tags "$url" '*.*.*' \
+    ls-remote --exit-code --refs --sort='version:refname' --tags "${url}" '*.*.*' \
     | tail --lines=1 )
-  echo "$results"
+  echo "${results}"
 }
 
 function split() {
   IFS=" " read -r hash version <<<"$1"
-  echo "$hash"
+  echo "${hash}"
 }
 
 function get-modulehash() {
   local temp
   temp=$(get-moduleinfo "$1")
-  split "$temp"
+  split "${temp}"
 }
 
 function get-url() {
   hash=$(get-modulehash "$1")
   IFS='/' read -r name provider module <<<"$1"
   url="git::https://github.com/${name}/terraform-${provider}-${module}.git?ref=${hash}"
-  echo "$url"
+  echo "${url}"
 }
--- hashurl.sh
+++ hashurl.sh
@@ -6,7 +6,7 @@ function get-moduleversion() {
  IFS='/' read -r name provider module <<<"$1"
  url="https://github.com/${name}/terraform-${provider}-${module}"
  git -c 'versionsort.suffix=-' \
-    ls-remote --exit-code --refs --sort='version:refname' --tags "$url" '*.*.*' \
+    ls-remote --exit-code --refs --sort='version:refname' --tags "${url}" '*.*.*' \
     | tail --lines=1 \
     | cut --delimiter='/' --fields=3
 }
@@ -16,28 +16,28 @@ function get-moduleinfo() {
   url="https://github.com/${name}/terraform-${provider}-${module}"
   #echo $url
   results=$(git -c 'versionsort.suffix=-' \
-    ls-remote --exit-code --refs --sort='version:refname' --tags "$url" '*.*.*' \
+    ls-remote --exit-code --refs --sort='version:refname' --tags "${url}" '*.*.*' \
     | tail --lines=1 )
-  echo "$results"
+  echo "${results}"
 }
 
 # shellcheck disable=SC2034
 function split() {
-  IFS=" " read -r hash version <<<"$1"
-  echo "$hash"
+  IFS=" " read -r hash version <<<"${1}"
+  echo "${hash}"
 }
 
 function get-modulehash() {
   local temp
-  temp=$(get-moduleinfo "$1")
-  split "$temp"
+  temp=$(get-moduleinfo "${1}")
+  split "${temp}"
 }
 
 function get-url() {
   hash=$(get-modulehash "$1")
   IFS='/' read -r name provider module <<<"$1"
   url="git::https://github.com/${name}/terraform-${provider}-${module}.git?ref=${hash}"
-  echo "$url"
+  echo "${url}"
 }
 
 # get-moduleversion $1
